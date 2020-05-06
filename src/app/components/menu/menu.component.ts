a32032bdc41741024a1f98b2667691ccc6af835c
Sutee <tee16357@gmail.com>
2020-05-07 03:41:48 +0700

    first commit b6025427

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  googleUrl: string;
+  facebookUrl: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.googleUrl = "http://www.google.com";
+    this.facebookUrl = "https://www.facebook.com/";
+  }
+  getUrlFacebook(){
+    return this.facebookUrl;
+  }
+}
